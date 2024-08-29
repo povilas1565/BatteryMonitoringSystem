3da3738e951c05ea2d26128fa2c93dfe30fea548
Paul156551 <pavel.ryzhkov.99@mail.ru>
2024-08-29 10:34:07 +0300

    Commit Changes

--- src/main/java/com/smartBattery/BatteryMonitoringtSystemApplication.java
+++ src/main/java/com/smartBattery/BatteryMonitoringtSystemApplication.java
@@ -4,10 +4,10 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class BatteryManagementSystemApplication {
+public class BatteryMonitoringtSystemApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(BatteryManagementSystemApplication.class, args);
+		SpringApplication.run(BatteryMonitoringtSystemApplication.class, args);
 	}
 
 }
