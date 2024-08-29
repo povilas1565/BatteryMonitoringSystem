3c13fcdccf84e2f4390893240161fc6769424dca
Paul156551 <pavel.ryzhkov.99@mail.ru>
2024-08-29 12:25:44 +0300

    Закончены репозитории

--- src/main/java/com/smartBattery/BatteryDataRepository.java
+++ src/main/java/com/smartBattery/BatteryDataRepository.java
@@ -7,19 +7,29 @@ import org.springframework.data.jpa.repository.Query;
 
 import java.time.LocalDateTime;
 import java.util.List;
+import java.util.Optional;
 
 public interface BatteryDataRepository extends JpaRepository<BatteryData, Integer> {
-
-    List<BatteryData> findByBatteryOrderByTimeStampDesc(Battery currentBattery);
 
     @Query("SELECT bd FROM BatteryData bd WHERE bd.battery.batteryId = :batteryId ORDER BY bd.timeStamp DESC")
     List<BatteryData> findLatestDataByBatteryId(Integer batteryId);
 
-    @Query("SELECT bd FROM BatteryData bd WHERE bd.battery.batteryId = :batteryId AND bd.timeStamp BETWEEN :startTime AND :endTime ORDER BY bd.timeStamp DESC")
-    List<BatteryData> findByBatteryIdAndTimeStampBetween(Integer batteryId, LocalDateTime startTime, LocalDateTime endTime);
 
-    // findLatestCurrentByBatteryId
-    // findLatestVoltageByBatteryId
-    // findLatestTemperatureByBatteryId
+    @Query("SELECT bd.voltage FROM BatteryData bd WHERE bd.battery.batteryId = :batteryId ORDER BY bd.timeStamp DESC")
+    Optional<Double> findLatestVoltageByBatteryId(Integer batteryId);
+
+
+    @Query("SELECT bd.current FROM BatteryData bd WHERE bd.battery.batteryId = :batteryId ORDER BY bd.timeStamp DESC")
+    Optional<Double> findLatestCurrentByBatteryId(Integer batteryId);
+
+    @Query("SELECT bd.temperature FROM BatteryData bd WHERE bd.battery.batteryId = :batteryId ORDER BY bd.timeStamp DESC")
+    Optional<Double> findLatestTemperatureByBatteryId(Integer batteryId);
+
+    @Query("SELECT bd FROM BatteryData bd WHERE bd.battery.batteryId = :batteryId AND bd.timeStamp BETWEEN :startTime AND :endTime ORDER BY bd.timeStamp DESC")
+    List<BatteryData> findRecordByBatteryIdAndTimeStampBetween(Integer batteryId, LocalDateTime startTime, LocalDateTime endTime);
+
+
+    List<BatteryData> findByBatteryOrderByTimeStampDesc(Battery currentBattery);
+
 
 }
