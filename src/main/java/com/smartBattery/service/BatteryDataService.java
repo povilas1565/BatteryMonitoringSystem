bf9855639f0650c8b0214d3cc14f239db5b19ac7
Paul156551 <pavel.ryzhkov.99@mail.ru>
2024-08-29 14:00:58 +0300

    Интерфейс BatteryDataService

---
+++ src/main/java/com/smartBattery/service/BatteryDataService.java
@@ -0,0 +1,21 @@
+package com.smartBattery.service;
+
+import com.smartBattery.exception.BatteryDataException;
+import com.smartBattery.exception.BatteryException;
+import com.smartBattery.model.BatteryData;
+
+import java.time.LocalDateTime;
+import java.util.List;
+
+public interface BatteryDataService {
+
+    List<BatteryData> getAllInfoOfBatteryId(Integer batteryId) throws Exception;
+
+    double getLatestVoltageRecord(Integer batteryId) throws BatteryException, BatteryDataException;
+
+    double getLatestCurrentRecord(Integer batteryId) throws BatteryException, BatteryDataException;
+
+    double getLatestTemperatureRecord(Integer batteryId) throws BatteryException, BatteryDataException;
+
+    List<BatteryData> trackRecordWithIdAndTimeStamp(Integer batteryId, LocalDateTime startTime, LocalDateTime endTime) throws BatteryException, BatteryDataException;
+}
